77e297e13b2dcd6e36da6ee4f8398e28aae03d5d
eufracia <ziporalicia@gmail.com>
2022-06-04 19:36:15 +0700

    Adding sort names function with callback

--- index.js
+++ index.js
@@ -6,15 +6,27 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+
+const sorter = (names, callbackSort) => {
+  const nameSort = callbackSort(names);
+  const arrNames = [];
+  for (let i = 0; i < nameSort.length; i++) {
+    arrNames.push(`${i + 1}. ${nameSort[i]}`);
+  }
+  return arrNames;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (names) => {
+  return names.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (names) => {
+  return names.sort().reverse();
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
